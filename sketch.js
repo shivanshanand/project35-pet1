2a39368c3a79a042eece46d8d2ad91797c11ffd2
shivanshanand <74009335+shivanshanand@users.noreply.github.com>
2021-01-04 11:51:41 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,62 +1,62 @@
-//Create variables here
-var happydog,dog,foodS,foodStock, database;
+  //Create variables here
+  var dog,foodS,foodStock, database;
+  var dogImg,dogImg2;
 
-function preload()
-{
-  //load images here
-  dogImg=loadImage("dogImg.png");
-  dogImg2=loadImage("dogImg1.png");
-}
-
-function setup() {
-  createCanvas(600, 500);
-
-
-  database=firebase.database();
-  foodStock=database.ref("Food");
-  foodStock.on("value",readStock);
-
-}
-
-function draw() {  
-  background(46,139,87);
-
-  if(keyDown===UP_ARROW){
-    writeStock(foodS);
-    dog.addImage(dogImg2);
+  function preload()
+  {
+    //load images here
+    dogImg=loadImage("dogImg.png");
+    dogImg2=loadImage("dogImg1.png");
   }
 
-  stroke("red");
-  fill("red")
-  textSize("90")
-  text("Note:Press UP_ARROW key to feed Drago Milk",170,30);
+  function setup() {
+    createCanvas(600, 500);
 
- /* fill("blue")
-  textSize("40")
-  text("Food Remaining:",260,200);
-  */
+    var dog1;
+    dog1.addImage(dogImg);
 
-  drawSprites();
-  //add styles here
-}
-
-function readStock( data ){
-
-  foodS=data.val();
-
-}
-
-function writeStock( x ){
-
-  if(x<=0){
-    x=0;
-  }
-  else{
-    x=x-1;
+    database=firebase.database();
+    foodStock=database.ref("Food");
+    foodStock.on("value",readStock);
   }
 
-  database.ref('/').update({
-    Food:x
-  })
+  function draw() {  
+    background(46,139,87);
 
-}
+    if(keyCode === UP_ARROW){
+      writeStock(foodS);
+      dog.addImage(dogImg2);
+    }
+
+    noStroke();
+    fill("white")
+    textSize("90")
+    text("Note:Press UP_ARROW key to feed Drago Milk",170,30);
+
+    fill("blue")
+    textSize("60")
+    text("Food Remaining:"+ foodS,170,200);
+
+    drawSprites();
+  }
+
+  function readStock( data ){
+
+    foodS=data.val();
+
+  }
+
+  function writeStock( x ){
+
+    if(x<=0){
+      x=0;
+    }
+    else{
+      x=x-1;
+    }
+
+    database.ref('/').update({
+      Food:x
+    })
+
+  }
